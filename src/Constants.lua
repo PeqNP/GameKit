79b9b362148b3770f15ca4f36aedda87ba1d2064
Eric Chamberlain <eric.chamberlain@hotmail.com>
2015-10-01 16:46:52 -0700

    Removing unnecessary constants not related to global scope

--- src/Constants.lua
+++ src/Constants.lua
@@ -1,19 +1,9 @@
 --[[
-  Game constants.
+  Global constants.
 
   @copyright 2014 Upstart Illustration LLC. All rights reserved.
 
 --]]
-
-BG_MUSIC_MAX_VOL = 0.6
-BG_MUSIC_MENU_VOL = 0.1
-FX_MAX_VOL = 0.8
-
--- The z-index of the main actor.
--- @fixme This is used in Actor in the flash() method. This needs to change.
--- It was done that way to ensure the user saw the flash, even if an actor below
--- the main actor was behind it.
-MAIN_ACTOR_Z = 50
 
 --[[ UI / HUD ]]--
 
