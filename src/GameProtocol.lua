3496d6453d51c398a8476b1ee445f3a912b40a15
Eric Chamberlain <eric.chamberlain@hotmail.com>
2015-12-08 09:23:13 -0800

    Adding protocol for games

---
+++ src/GameProtocol.lua
@@ -0,0 +1,10 @@
+--
+-- Required methods necessary to implement for a GameTools game.
+--
+-- @copyright (c) 2015 Upstart Illustration LLC. All rights reserved.
+--
+
+GameProtocol = Protocol(
+    Method("start")
+  , Method("stop")
+)
