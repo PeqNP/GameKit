98d794e8849cf276b1271f496f26e6c4c018fe11
Eric Chamberlain <eric.chamberlain@hotmail.com>
2015-12-22 12:58:57 -0800

    BridgeResponse now accepts ID

--- src/bridge/BridgeResponse.lua
+++ src/bridge/BridgeResponse.lua
@@ -2,23 +2,32 @@
 -- @copyright (c) 2015 Upstart Illustration LLC. All rights reserved.
 --
 
+require "bridge.BridgeResponseProtocol"
+
 BridgeResponse = Class()
+BridgeResponse.implements(BridgeResponseProtocol)
 
 function BridgeResponse.new(self)
     local success
+    local id
     local _error
 
-    function self.init(_success, _e)
+    function self.init(_success, _id, _e)
         if _success == 1 or _success == true then
             success = true
         else
             success = false
         end
+        id = _id
         _error = _e
     end
 
     function self.isSuccess()
         return success
+    end
+
+    function self.getId()
+        return id
     end
 
     function self.getError()
