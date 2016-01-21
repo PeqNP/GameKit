072f3f0a2edc84f53566f1ceff1c5d2ae4c87999
Eric Chamberlain <eric.chamberlain@hotmail.com>
2016-01-21 13:11:32 -0800

    Adding factory used to create any feature

---
+++ src/FeatureFactory.lua
@@ -0,0 +1,65 @@
+--
+-- @copyright (c) 2016 Upstart Illustration LLC. All rights reserved.
+--
+
+local FeatureFactory = Class()
+
+function FeatureFactory.new(self)
+    local platform
+    local bridge
+
+    function self.init(_platform)
+        platform = _platform
+    end
+
+    local function getBridge()
+        if bridge then
+            return bridge
+        end
+        require "bridge.Bridge"
+        require "bridge.BridgeAdaptor"
+        platform = BridgeAdaptor.getAdaptor(device.platform)
+        bridge = Bridge(platform)
+        return bridge
+    end
+
+    local function getAppManager()
+        local appModule = require("bridge.modules.app")
+        appModule.init(getBridge())
+
+        local AppManager = require("app.AppManager")
+        return AppManager(appModule)
+    end
+
+    local function getIAP(tickets)
+        local iapModule = require("bridge.modules.iap")
+        iapModule.init(getBridge())
+
+        local IAPManager = require("iap.Manager")
+        local iapManager = IAPManager(iapModule)
+
+        local IAP = require("iap.IAP")
+        return IAP(iapManager, tickets)
+    end
+
+    local function getAdManager(adConfig, networks, adServer)
+        local adModule = require("bridge.modules.ad")
+        adModule.init(getBridge())
+
+        local service
+        if adServer then
+            require("mediation.MediationService")
+            service = MediationService(adServer.getHost(), adServer.getPort(), adServer.getPath())
+        end
+
+        local AdServerManager = require("ad.AdServerManager")
+        return AdServerManager(adModule, adConfig, networks, service)
+    end
+
+    local function getSocialManager()
+        local SocialManager = require("social.Manager")
+        return SocialManager(getBridge())
+    end
+end
+
+return FeatureFactory
