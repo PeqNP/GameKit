384a6f5c994497f4191724d630260a93ae893590
Eric Chamberlain <eric.chamberlain@hotmail.com>
2016-02-10 12:00:04 -0800

    load_iap_config now returns an IAPConfig to track the platform the IAP config is for. This is in preparation of the Android IAP Lua code generation logic.

--- bin/ugf/iap.py
+++ bin/ugf/iap.py
@@ -5,6 +5,20 @@
 import json
 import os
 
+class IAPConfig (object):
+    def __init__(self, platform, tickets):
+        self.platform = platform
+        self.tickets = tickets
+
+    def getPlatform(self):
+        return self.platform
+
+    def ticketsToLua(self, separator):
+        lua_tickets = []
+        for ticket in self.tickets:
+            lua_tickets.append(ticket.toLua())
+        return separator.join(lua_tickets)
+
 class IAPTicket (object):
     def __init__(self, productId, sku):
         self.productId = productId
@@ -13,7 +27,7 @@ class IAPTicket (object):
     def toLua(self):
         return "Ticket(\"{}\", \"{}\")".format(self.productId, self.sku)
 
-def load_iap_config(path):
+def load_iap_config(platform, path):
     if not os.path.isfile(path):
         raise IOError("IAP config file does not exist at path {}".format(path))
     fh = open(path, "r")
@@ -23,5 +37,5 @@ def load_iap_config(path):
     json_tickets = json.loads(json_blob)
     for ticket in json_tickets:
         tickets.append(IAPTicket(ticket[0], ticket[1]))
-    return tickets
+    return IAPConfig(platform, tickets)
 
