472291a0f9d96e46848cc663c66d1f2819ad2d4a
Eric Chamberlain <eric.chamberlain@hotmail.com>
2016-08-18 20:29:36 -0700

    NTP client nearly complete. Adding signal script to be able to execute any script with the signal libs.

---
+++ bin/ntp.lua
@@ -0,0 +1,15 @@
+--
+-- Prints current time from NTP server.
+--
+-- @copyright (c) 2016 Upstart Illustration, LLC
+--
+
+require("lang.Signal")
+require("Logger")
+
+local NTPClient = require("ntp.Client")
+local client = NTPClient()
+response = client.requestTime()
+Log.i("NTPResponse: date (%s) time (%s) success (%s) error (%s)", response.getDate(), response.getEpochTime(), response.isSuccess(), response.getError())
+
+os.exit(0)
--- src/ntp/Client.lua
+++ src/ntp/Client.lua
@@ -2,7 +2,12 @@
 -- @copyright (c) 2016 Upstart Illustration, LLC
 --
 
+local NTPResponse = require("ntp.Response")
+
 local Client = Class()
+
+local TIME_SERVER_HOST = "time.nist.gov"
+local TIME_SERVER_PORT = 13
 
 -- 0.north-america.pool.ntp.org
 -- us.pool.ntp.org
@@ -12,19 +17,20 @@ function Client.new(self)
     local port
 
     function self.init(h, p)
-        host = h or "us.pool.ntp.org"
-        port = p or 13
+        host = h or TIME_SERVER_HOST
+        port = p or TIME_SERVER_PORT
     end
 
     function self.requestTime()
-        local timeserver, err = socket.tcp()
+        local client, err = socket.tcp()
         if err then
-            return NTPResponse(nil, false, "Could not create TCP socket: "..err)
+            return NTPResponse(nil, false, "Could not create TCP socket: " .. err)
         end
 
-        timeserver:settimeout(0.10, "t")
-        timeserver:connect(host, port)
-        timeserver:settimeout(0.10, "t")
+        Log.i("NTP.client.requestTime: Connecting to NTP @ %s:%s", host, port)
+
+        client:settimeout(2.0, "t")
+        client:connect(host, port)
         
         --[[ Where is the 'err' coming from?
         if err then
@@ -32,25 +38,26 @@ function Client.new(self)
         end
         ]]--
         
-        local _, err = timeserver:send("\n")
+        local _, err = client:send("\n")
 
         if err then
-            return NTPResponse(nil, false, "Error in sending: "..err)
+            return NTPResponse(nil, false, "Error in sending: " .. err)
         end
         
         -- KMW: need to rx a line, otherwise error.
-        local _, _ = timeserver:receive('*l')
-        local line, err = timeserver:receive('*l')
+        local _, _ = client:receive('*l')
+        local line, err = client:receive('*l')
+        client:close()
 
         if err then
-            return NTPResponse(nil, false, "Error in receiving: "..err)
+            return NTPResponse(nil, false, "Error in receiving: " .. err)
         end
         
         if string.find(line, "UTC") then
             return NTPResponse(line, true, nil)
         end
 
-        return NTPResponse(nil, false, "Bad result from server: "..err)
+        return NTPResponse(nil, false, "Bad result from server: " .. err)
     end
 end
 
