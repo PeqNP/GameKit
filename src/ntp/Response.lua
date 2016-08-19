17e3a32cadd37685bd6009c1e4ef5284f67b0caf
Eric Chamberlain <eric.chamberlain@hotmail.com>
2016-08-19 09:23:34 -0700

    Return current time if epoch compute fails

--- src/ntp/Response.lua
+++ src/ntp/Response.lua
@@ -48,7 +48,12 @@ function Response.new(self)
         parts = table.slice(parts, 1, 3)
         local date_only = table.join(parts, " ")
         local ms, year, month, day, hour, minute, second, _, _, _, _, _ = date_only:match(date_format)
-        return os.time({day=lz(day), month=lz(month), year=century(year), hour=lz(hour), min=lz(minute), sec=lz(second)})
+        local epoch = os.time({day=lz(day), month=lz(month), year=century(year), hour=lz(hour), min=lz(minute), sec=lz(second)})
+        if not epoch then
+            Log.w("NTP.Response.getEpoch: Failed to compute epoch for date (%s)", date_only)
+            return gettime()
+        end
+        return epoch
     end
 
     function self.isSuccess()
